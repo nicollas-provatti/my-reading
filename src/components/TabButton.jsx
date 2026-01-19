f29dd6cd411a71d558b6e9c47f2bd4c5e96456ec
nicollas-provatti <npg@ic.ufal.br>
2026-01-19 08:22:08 -0300

    refactor: simplify Button class logic

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -4,15 +4,12 @@ import { FilterContext } from "../store/filter-context";
 function Button({ filter: selectedFilter, children }) {
   const { filter, changeFilter } = useContext(FilterContext);
 
-  let classesButton =
-    "px-4 py-2 rounded-full font-semibold text-sm cursor-pointer text-zinc-500 transition-collors duration-300 hover:bg-zinc-100 sm:text-lg";
+  const isActive = selectedFilter === filter;
 
-  if (selectedFilter === filter) {
-    classesButton =
-      "px-4 py-2 rounded-full font-semibold text-sm cursor-pointer text-black/80 bg-zinc-200 sm:text-lg";
-  }
-
-  // Obs.: tive que repetir "px-4 py-2 rounded-full font-semibold cursor-pointer", pois eu não quero o hover quando o botão estiver seleciondo.
+  const classesButton = `
+    px-4 py-2 rounded-full font-semibold text-sm cursor-pointer 
+    transition-colors duration-300 sm:text-lg 
+    ${isActive ? "text-black/80 bg-zinc-200" : "text-zinc-500 hover:bg-zinc-100"}`;
 
   return (
     <button
