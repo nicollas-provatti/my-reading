9920644f3cdf7b6f170d00735baac7b253e4e74b
nicollas-provatti <npg@ic.ufal.br>
2026-01-24 14:41:37 -0300

    feat(ui): improve error feedback with visual messag

--- src/components/books/Bookcase.jsx
+++ src/components/books/Bookcase.jsx
@@ -1,12 +1,13 @@
 import { useBooks } from "../../store/books/use-books";
 import Book from "./Book";
 import Spinner from "../UI/Spinner";
+import Error from "../UI/Error";
 
 function Bookcase() {
   const { books, isFetching, error } = useBooks();
 
   if (isFetching) return <Spinner text="Carregando livros..." />;
-  if (error) return <p className="text-center">{error}</p>
+  if (error) return <Error error={error}/>
 
   return (
     <div className="grid grid-cols-1 gap-8 p-4 sm:grid-cols-2 lg:grid-cols-4">
