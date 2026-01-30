5889c7a0854c836e7f325eb722c247e103da5aeb
nicollas-provatti <npg@ic.ufal.br>
2026-01-30 11:12:48 -0300

    fix(auth): ajusta redirect de logout após mudança da rota de login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,6 @@ function Header() {
   const { logout } = useAuth();
   return (
     <header className="relative py-3 min-h-50 md:min-h-65 bg-[url('/library.jpg')] bg-center bg-cover shadow-md">
-      {/* overlay gradient */}
       <div className="absolute inset-0 bg-linear-to-b from-black/70 via-black/40 to-transparent" />
 
       <div className="relative h-full flex items-end">
